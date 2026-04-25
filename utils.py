cffee005341237b9bf756b72c4bf948dbf94c69d
Umaparvathy C S <umaparvathycs47@gmail.com>
2026-04-25 22:00:34 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def create_directories():
-    """Create the necessary directory structure for the project workflow."""
-    dirs = ['data', 'models', 'notebooks', 'src']
-    for d in dirs:
-        os.makedirs(d, exist_ok=True)
